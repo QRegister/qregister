917743074929ce633ab27cbd7d69542e7ac6ffe8
kasmurat <muratkas314@gmail.com>
2021-03-07 18:54:19 +0300

    getReceiptFromHash function is completed

--- lib/src/data/repositories/data_inventory_repository.dart
+++ lib/src/data/repositories/data_inventory_repository.dart
@@ -11,73 +11,107 @@ class DataInventoryRepository implements InventoryRepository {
 
   factory DataInventoryRepository() => _instance;
 
-  List<List<dynamic>> csvData;
+  List<List<dynamic>> inventoryCsvData;
+  List<List<dynamic>> storeCsvData;
 
   @override
-  Future<Receipt> getReceiptFromHash(String hash) async {List<String> backwards_num_item=[];
-List<String> backwards_amount_item=[];
-int start_index=0;
-List<String> the_rest=[];
+  Future<Receipt> getReceiptFromHash(String hash) async {
+    List<String> itemCodeList = [];
+    List<String> itemAmountList = [];
+    List<String> otherInfosList = [];
+    int index = 0;
 
-for(int x=0; x<hesh.length; x++){
-    
-    if(hesh[x]=="#"){
-        the_rest.add(hesh.substring(start_index,x));
-        start_index=x+1;
-    
-    
+    for (int x = 0; x < hash.length; x++) {
+      if (hash[x] == "#") {
+        otherInfosList.add(hash.substring(index, x));
+        index = x + 1;
+      } else if (hash[x] == "?") {
+        itemCodeList.add(hash.substring(index, x));
+        index = x + 1;
+      } else if (hash[x] == "%") {
+        itemAmountList.add(hash.substring(index, x));
+        index = x + 1;
+      }
     }
-  
-    else if (hesh[x]=="?"){
-        backwards_num_item.add(hesh.substring(start_index,x));
-        start_index=x+1;}
 
+    List<Product> productList = [];
 
-    else if (hesh[x]=="%"){
-        backwards_amount_item.add(hesh.substring(start_index,x));
-        start_index=x+1;}
-}
+    if (itemCodeList != null && itemCodeList.length != 0) {
+      itemCodeList.forEach((itemCode) {
+        inventoryCsvData.forEach((data) {
+          if (data[0] == itemCode) {
+            productList.add(
+              Product(
+                barcode: data[1],
+                name: data[2],
+                unitPrice: data[3],
+                unitOfMeasurement: data[4],
+                taxRate: data[5],
+                count: double.tryParse(
+                    itemAmountList.elementAt(itemCodeList.indexOf(itemCode))),
+              ),
+            );
+          }
+        });
+      });
+    }
 
-print(backwards_num_item);
-print(backwards_amount_item);
- 
-int date= int.parse(the_rest[0]);
-int store_id=int.parse(the_rest[1]);
-int cashier_id=int.parse(the_rest[2]);
-String receipt_id=the_rest[3];
+    double totalPrice = 0;
+    double totalTax = 0;
 
-print("Cahier ID: ${cashier_id}");
-print("Cashier Name is: ${cashier_names[cashier_id-1]}");
-print(date);
-print(store_id); 
-print("receipt ID: ${receipt_id}");
+    productList.forEach((product) {
+      double productTotalPrice = product.unitPrice * product.count;
+      totalPrice += productTotalPrice;
+      totalTax += productTotalPrice * product.taxRate;
+    });
 
-double total=0;
+    DateTime date =
+        DateTime.fromMillisecondsSinceEpoch(int.tryParse(otherInfosList[0]));
+    String receiptId = otherInfosList[3];
+    String cashierName = otherInfosList[2];
 
+    int storeId = int.parse(otherInfosList[1]);
+    int storeIndex;
 
-for(int x=0; x<cashier_names.length; x++){}
+    storeCsvData.forEach((store) {
+      if (store[0] == storeId) {
+        storeIndex = storeCsvData.indexOf(store);
+      }
+    });
 
+    String storeSlug = storeCsvData.elementAt(storeIndex).elementAt(1);
+    String storeName = storeCsvData.elementAt(storeIndex).elementAt(2);
+    String storeLocation = storeCsvData.elementAt(storeIndex).elementAt(3);
+    String storeLocationAddress =
+        storeCsvData.elementAt(storeIndex).elementAt(4);
 
-
-for(int x=0; x<backwards_num_item.length; x++){
-    for(int y=0; y<item_code.length; y++){
-        if ((item_code[y]) == int.parse(backwards_num_item[x])){
-
-            print("item name: ${item_name[y]}");
-            print("price: ${item_prize[y].toString()}  ${item_prize_type[y].toString()}, amount: ${backwards_amount_item[x]}");
-            total=total+double.parse(backwards_amount_item[x])*item_prize[y];
-}}}
-
-print("Your TOTAL is: ${total.toString()} TL");
-
-}}
+    return Receipt(
+      id: receiptId,
+      cashierName: cashierName,
+      storeName: storeName,
+      storeSlug: storeSlug,
+      date: date,
+      products: productList,
+      storeLocation: storeLocation,
+      storeLocationAddress: storeLocationAddress,
+      totalPrice: totalPrice,
+      totalTax: totalTax,
+    );
+  }
 
   @override
   Future<void> readInventoryCsv() async {
-    final csvString = await rootBundle.loadString('assets/files/inventory.csv');
-    final List<List<dynamic>> convertedCsvData =
-        CsvToListConverter().convert(csvString);
+    final inventoryCsvString =
+        await rootBundle.loadString('assets/files/inventory.csv');
+    final List<List<dynamic>> convertedInventoryCsvData =
+        CsvToListConverter().convert(inventoryCsvString);
 
-    this.csvData = convertedCsvData;
+    final storeCsvString =
+        await rootBundle.loadString('assets/files/stores.csv');
+    final List<List<dynamic>> convertedStoresCsvData =
+        CsvToListConverter().convert(storeCsvString);
+
+    this.inventoryCsvData = convertedInventoryCsvData;
+    this.storeCsvData = convertedStoresCsvData;
   }
 }
